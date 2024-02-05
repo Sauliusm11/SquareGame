75454bfa2c86cb5f0e1f745ecc547826a8cce971
Sauliusm11 <88769620+Sauliusm11@users.noreply.github.com>
2024-02-05 14:19:28 +0200

    Tile placement refactoring

--- Assets/Scripts/TilePlacement.cs
+++ Assets/Scripts/TilePlacement.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 
-public class TilePlacement : MonoBehaviour, IDragHandler
+public class TilePlacement : MonoBehaviour, IDragHandler, IPointerClickHandler
 {
     Tilemap tilemap;
     GameManager manager;
     SelectionHandler selectionHandler;
-    bool overUI = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +22,25 @@ public class TilePlacement : MonoBehaviour, IDragHandler
     {
 
     }
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)
     {
-        overUI = false;
+        PlaceTile(eventData.pointerCurrentRaycast.worldPosition);
+    }
+    public void PlaceTile(Vector3 position)
+    {        
+        //Build eventData from Input pos;
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+
+        if (!PointerOverUI(eventData))
+        { 
+            Vector3Int cellPosition = tilemap.WorldToCell(position);
+            Tile tile = selectionHandler.GetTileFromSelection(manager.GetSelectedSquare());
+            tilemap.SetTile(cellPosition, tile);
+        }
+    }
+    bool PointerOverUI(PointerEventData eventData)
+    {
         List<RaycastResult> raycastResults = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventData, raycastResults);
         for (int index = 0; index < raycastResults.Count; index++)
@@ -34,37 +49,10 @@ public class TilePlacement : MonoBehaviour, IDragHandler
             Debug.Log(curRaysastResult.gameObject.layer);
             if (curRaysastResult.gameObject.layer == 5)//5 is UI value
             {
-                overUI = true;
-                break;
+                return true;
             }
         }
-        if(!overUI)
-            PlaceTile(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-
-    }
-    public void PlaceTile(Vector3 position)
-    {
-        overUI = false;
-        PointerEventData eventData = new PointerEventData(EventSystem.current);
-        eventData.position = Input.mousePosition;
-        List<RaycastResult> raycastResults = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventData, raycastResults);
-        for (int index = 0; index < raycastResults.Count; index++)
-        {
-            RaycastResult curRaysastResult = raycastResults[index];
-            if (curRaysastResult.gameObject.layer == 5)//5 is UI value
-            {
-                overUI = true;
-                break;
-            }
-        }
-        if (!overUI)
-        { 
-            Vector3Int cellPosition = tilemap.WorldToCell(position);
-            Tile tile = selectionHandler.GetTileFromSelection(manager.GetSelectedSquare());
-            tilemap.SetTile(cellPosition, tile);
-            //Debug.Log(cellPosition);
-        }
+        return false;
     }
     public void OnDrag(PointerEventData eventData)
     {
