ede1d033cfca91bd32b3752efd41c1f228d5e1c2
Sauliusm11 <88769620+Sauliusm11@users.noreply.github.com>
2024-02-10 14:11:32 +0200

    Implemented diagonal arrow right rules
    Fixed naming inconsistency too

---
+++ Assets/Scripts/TileRules/DiagonalArrowRightRules.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+internal class DiagonalArrowRules : AbstractTileRule
+{
+    public DiagonalArrowRules(Tile tile)
+    {
+        Tile = tile;
+    }
+
+    bool CheckLocation(int x, int y, TileBase[] tiles, int xSize, int ySize)
+    {
+        if (tiles[x + y * xSize] != null)
+        {
+            if (tiles[x + y * xSize].name.Contains("Empty"))
+                return true;
+            else
+                return false;
+        }
+        return true;
+    }
+    public override bool ProcessRule(Vector2Int location, TileBase[] tiles, Vector2Int bounds)
+    {
+        int x = location.x;
+        int currentX = location.x;
+        int y = location.y;
+        int currentY = location.y;
+        int xSize = bounds.x;
+        int ySize = bounds.y;
+        while(x+1 < xSize && y + 1 < ySize)
+        {
+            x += 1;
+            y += 1;
+            if(!CheckLocation(x,y,tiles,xSize,ySize))
+            {
+                Debug.Log(tiles[x + y * xSize]);
+                return false;
+            }
+        }
+        x = currentX;
+        y = currentY;
+        while(x > 0 && y > 0)
+        {
+            x -= 1;
+            y -= 1;
+            if(!CheckLocation(x,y,tiles,xSize,ySize))
+            {
+                Debug.Log(tiles[x + y * xSize]);
+                return false;
+            }
+        }
+        return true;
+    }
+}
