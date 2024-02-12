462d1fd944a0acfa589f9f3fba6eb6ba2f31c271
Sauliusm11 <88769620+Sauliusm11@users.noreply.github.com>
2024-02-12 12:17:29 +0200

    Implemented Cross rules

--- Assets/Scripts/TileRules/CrossRules.cs
+++ Assets/Scripts/TileRules/CrossRules.cs
@@ -7,9 +7,86 @@ internal class CrossRules : AbstractTileRule
     {
         Tile = tile;
     }
-
+    bool CheckLocation(int x, int y, TileBase[] tiles, int xSize, int ySize)
+    {
+        if (tiles[x + y * xSize] != null)
+        {
+            if (tiles[x + y * xSize].name.Contains("Empty"))
+                return false;
+            else
+                return true;
+        }
+        //Change this to true to allow placing at the edges
+        return false;
+    }
     public override bool ProcessRule(Vector2Int location, TileBase[] tiles, Vector2Int bounds)
     {
-        throw new System.NotImplementedException();
+        int x = location.x;
+        int currentX = location.x;
+        int y = location.y;
+        int currentY = location.y;
+        int xSize = bounds.x;
+        int ySize = bounds.y;
+        bool empty = true;
+        if (currentY > 0)
+        {
+            y = currentY - 1;
+            if (CheckLocation(x, y, tiles, xSize, ySize))
+                empty = false;
+        }
+        
+
+        y = currentY;
+        x = currentX + 1;
+        if (x < xSize && CheckLocation(x, y, tiles, xSize, ySize))
+        {
+            if (empty)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if(!empty)
+            {
+                return false;
+            }
+        }   
+        x = currentX - 1;
+        if (x > -1 && CheckLocation(x, y, tiles, xSize, ySize))
+        {
+            if (empty)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (!empty)
+            {
+                return false;
+            }
+        }
+
+        y = currentY + 1;
+        if (y < ySize)
+        {
+            x = currentX;
+            if (CheckLocation(x, y, tiles, xSize, ySize))
+            {
+                if (empty)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (!empty)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
