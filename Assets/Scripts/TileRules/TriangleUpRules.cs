454026ca88a63a120cc98b14f11f231e0061e692
Sauliusm11 <88769620+Sauliusm11@users.noreply.github.com>
2024-02-12 12:01:55 +0200

    Implemented updated Triangle rules

--- Assets/Scripts/TileRules/TriangleUpRules.cs
+++ Assets/Scripts/TileRules/TriangleUpRules.cs
@@ -13,10 +13,43 @@ class TriangleUpRules : AbstractTileRule
     {
         Tile = tile;
     }
-
+    bool CheckLocation(int x, int y, TileBase[] tiles, int xSize, int ySize)
+    {
+        if (tiles[x + y * xSize] != null)
+        {
+            if (tiles[x + y * xSize].name.Contains("Empty"))
+                return false;
+            else
+                return true;
+        }
+        //Change this to true to allow placing at the edges
+        return true;
+    }
     public override bool ProcessRule(Vector2Int location, TileBase[] tiles, Vector2Int bounds)
     {
-        throw new NotImplementedException();
+        int x = location.x;
+        int currentX = location.x;
+        int y = location.y;
+        int currentY = location.y;
+        int xSize = bounds.x;
+        int ySize = bounds.y;
+        y = currentY + 1;
+        if (y < ySize)
+        {
+            if (!CheckLocation(x, y, tiles, xSize, ySize))
+                return false;
+        }
+        if (currentY > 0)
+        {
+            y = currentY - 1;
+            x = currentX - 1;
+            if (x > -1 && !CheckLocation(x, y, tiles, xSize, ySize))
+                return false;
+            x = currentX + 1;
+            if (x < xSize && !CheckLocation(x, y, tiles, xSize, ySize))
+                return false;
+        }
+        return true;
     }
 }
 
